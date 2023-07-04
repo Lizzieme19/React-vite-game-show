04cc226c4c02f116623984676b190106f1a876aa
Lizzieme19 <lizziekamash@gmail.com>
2023-07-04 15:43:00 +0300

    Components folder added

--- src/Message.tsx
+++ src/Message.tsx
@@ -2,10 +2,9 @@
 //PascalCasing
 function Message(){
     //JSX: Javascript XML
-    const name= ""
+    const name= "Lizzie"
     if (name)
  return <h1>Hello {name}</h1>;
  return <h1> Hello World</h1>;
 }
 
-export default Message;
